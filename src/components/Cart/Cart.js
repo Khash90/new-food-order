58806f70635910a90d91d5bddd9d9107f5887216
Khash <khashayar.m.003@gmail.com>
2023-12-23 10:35:00 +0330

    user information successfuly sent to DB

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,7 +8,8 @@ import Checkout from './Checkout';
 
 const Cart = (props) => {
   
-  const [isCheckout, setIsCheckout] = useState(false)
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const cartCtx = useContext(CartContext);
 
@@ -25,6 +26,16 @@ const Cart = (props) => {
 
   const orderHandler = () => {
     setIsCheckout(true)
+  }
+
+  const submitOrderHandler = (userData) => {
+     fetch('https://http-ead69-default-rtdb.firebaseio.com/orders.json' , {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    });
   }
 
   const cartItems = (
@@ -57,7 +68,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
